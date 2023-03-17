426434d999a966d7d1f25049a8cffcce084d5fa2
easymikey <mikhail.avdeev@vk.team>
2023-03-17 19:00:46 +0300

    refactor: delete unused code

--- src/repodata/mod.rs
+++ src/repodata/mod.rs
@@ -13,11 +13,13 @@ use crate::config;
 use anyhow::Result;
 use std::{fs, os::unix::prelude::PermissionsExt};
 use tempfile::Builder;
-use url::Url;
 
 pub async fn download_images(config: config::Config) -> Result<()> {
-    let target_url_origin = Url::parse(&config.repodata.target_url.origin)?;
-    let meta_data_url = target_url_origin.join(&config.repodata.target_url.index_uri)?;
+    let meta_data_url = config
+        .repodata
+        .target_url
+        .origin
+        .join(&config.repodata.target_url.index_uri)?;
 
     let lxc_image_metadata_collection = LXCImageMetadataCollection::of(&meta_data_url)
         .get()
@@ -26,8 +28,10 @@ pub async fn download_images(config: config::Config) -> Result<()> {
 
     for (lxc_container_metadata, post_process) in lxc_image_metadata_collection {
         let out_tempdir_path = Builder::new().prefix(".repodata_").tempdir()?;
-        let path = lxc_container_metadata.path.trim_start_matches("/");
-        let out_dir_path = &config.repodata.host_root_dir.join(&path);
+        let out_dir_path = &config
+            .repodata
+            .host_root_dir
+            .join(&lxc_container_metadata.path);
 
         if out_dir_path.exists() {
             continue;
@@ -36,24 +40,18 @@ pub async fn download_images(config: config::Config) -> Result<()> {
         fs::create_dir_all(&out_dir_path)?;
 
         for image_file in &config.repodata.image_files {
-            let download_url = target_url_origin.join(&path)?.join(&image_file)?;
+            let download_url = config
+                .repodata
+                .target_url
+                .origin
+                .join(&lxc_container_metadata.path)?
+                .join(&image_file)?;
             let tempfile = download_image(download_url).await?;
 
-            let is_filename_rootfs = image_file == "rootfs.tar.xz";
-
-            if is_filename_rootfs {
+            if image_file == "rootfs.tar.xz" {
                 if let Some(post_process) = &post_process {
                     patch_image(
                         post_process,
-                        &tempfile,
-                        config.repodata.patcher_timeout,
-                        &lxc_container_metadata,
-                    )?;
-                }
-
-                if let Some(post_script) = &config.repodata.post_script_path {
-                    patch_image(
-                        post_script,
                         &tempfile,
                         config.repodata.patcher_timeout,
                         &lxc_container_metadata,
