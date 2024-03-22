5bd15cfe7017ee2f996dc7f7b624f7fcbe2b5a0d
Alexander Sidorov <mrbit73@gmail.com>
2024-03-22 19:46:54 +0400

    Fix: Bugs in cleanup_image_entries (#26)
    * fix: don't skip first item and infinite loop when remove skipped (if root_dir is symlink)
    ---------
    
    Co-authored-by: Alexander Sidorov <aleks.sidorov@vk.team>

--- src/repodata/lxc_image_entries_cleanup.rs
+++ src/repodata/lxc_image_entries_cleanup.rs
@@ -6,43 +6,46 @@ use std::{collections::HashMap, fs, path::PathBuf, time::Duration};
 
 pub fn cleanup_image_entries(
     root_dir: &PathBuf,
-    number_of_container_to_backup: i16,
+    number_of_container_to_backup: usize,
     image_entries: Vec<(LXCImageMetadata, Duration)>,
 ) -> Result<()> {
     info!("Cleanup LXC images started.");
 
+    let root_dir = root_dir.canonicalize()?;
     let hashed_image_entries: HashMap<_, Vec<_>> =
         image_entries
             .into_iter()
             .fold(HashMap::new(), |mut acc, (image_meta, mtime)| {
-                acc.entry((
-                    image_meta.dist.clone(),
-                    image_meta.release.clone(),
-                    image_meta.arch.clone(),
-                    image_meta.type_.clone(),
-                ))
-                .and_modify(|entries| {
-                    entries.push((image_meta.path.to_owned(), mtime.to_owned()));
-                })
-                .or_insert(Vec::new());
+                let image_entries = acc
+                    .entry((
+                        image_meta.dist,
+                        image_meta.release,
+                        image_meta.arch,
+                        image_meta.type_,
+                    ))
+                    .or_default();
+                image_entries.push((image_meta.path, mtime));
 
                 acc
             });
 
     for mut image_entries in hashed_image_entries.into_values() {
+        if image_entries.len() <= number_of_container_to_backup {
+            continue;
+        }
+
+        let number_of_container_to_remove = image_entries.len() - number_of_container_to_backup;
         image_entries.sort_by(|a, b| a.1.cmp(&b.1));
 
         info!("cleanup_image_entries: {:#?}", image_entries);
 
-        while image_entries.len() > number_of_container_to_backup as usize {
-            let (removed_dir, _) = &image_entries[0];
-            if removed_dir.canonicalize()?.starts_with(root_dir) {
+        for (removed_dir, _) in image_entries.iter().take(number_of_container_to_remove) {
+            if removed_dir.canonicalize()?.starts_with(&root_dir) {
                 fs::remove_dir_all(removed_dir)?;
                 info!(
                     "Remove LXC image directory. Directory path: {:?}",
                     removed_dir
                 );
-                image_entries.remove(0);
             }
         }
     }
